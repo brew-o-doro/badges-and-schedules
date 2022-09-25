a50b4d21e4fc7e76b40b97c50d30558f722bc86f
Teodoro <tedtke514@gmail.com>
2022-09-24 21:15:22 -0500

    commit tree 5c41eca184425c8f26d5c9911ca5c361df73e2f9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+ def  batch_badge_creator(attendees)
+    badges = []
+    attendees.each{|badge|  badges << "Hello, my name is #{badge}."}
+    return badges
+ end
+ 
+ #What we need to do here is iterate through the array containing the list of attendees, 
+ #create the room assignment strings for each speaker, and return a new array containing the messages
+ def assign_rooms(attendees)
+    new_list = []
+    attendees.each_with_index { |speaker, i| new_list << "Hello, #{speaker}! You'll be assigned to room #{i + 1}!"}
+    return new_list
+ end
+ 
+ def printer(attendees)
+    batch_badge_creator(attendees).each{|badge| puts badge}
+    assign_rooms(attendees).each { |assignment| puts assignment }
+ end
